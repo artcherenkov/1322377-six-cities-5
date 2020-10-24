68f65eab48f084a58d7bde880a8142dabab42935
Артем Черенков <art.cherenkov@gmail.com>
2020-10-24 21:54:53 +0700

    Добавляет action'ы изменения сортировки и активного объявления

--- src/store/action.js
+++ src/store/action.js
@@ -1,7 +1,9 @@
 export const ActionType = {
   CHANGE_CITY: `CHANGE_CITY`,
   GET_OFFERS: `GET_OFFERS`,
-  GET_CITY_OFFERS: `GET_CITY_OFFERS`
+  GET_CITY_OFFERS: `GET_CITY_OFFERS`,
+  CHANGE_SORT_TYPE: `CHANGE_SORT_TYPE`,
+  CHANGE_ACTIVE_OFFER: `CHANGE_ACTIVE_OFFER`
 };
 
 export const ActionCreator = {
@@ -9,12 +11,22 @@ export const ActionCreator = {
     type: ActionType.CHANGE_CITY,
     payload: newCity,
   }),
+
   getOffersList: () => ({
-    type: ActionType.GET_OFFERS,
+    type: ActionType.GET_OFFERS
   }),
-  getCityOffers: () => {
-    return ({
-      type: ActionType.GET_CITY_OFFERS
-    });
-  }
+
+  getCityOffers: () => ({
+    type: ActionType.GET_CITY_OFFERS
+  }),
+
+  changeSortType: (newSortType) => ({
+    type: ActionType.CHANGE_SORT_TYPE,
+    payload: newSortType
+  }),
+
+  changeActiveOffer: (newActiveOffer) => ({
+    type: ActionType.CHANGE_ACTIVE_OFFER,
+    payload: newActiveOffer
+  })
 };
