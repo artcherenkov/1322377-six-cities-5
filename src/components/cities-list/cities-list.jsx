6283d387456da6966dfd1ce7f7b3c6f149212e35
Артем Черенков <art.cherenkov@gmail.com>
2020-10-26 17:34:06 +0700

    Оптимизирует вычисление классов для CitiesList

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import {Cities} from '../../const';
 import {toCamelCase} from "../../utils/common";
 import {nanoid} from "nanoid";
 
-const CitiesList = ({city: currentCity, onCityChange}) => (
-  <div className="tabs">
-    <section className="locations container">
-      <ul className="locations__list tabs__list">
-        {Object.values(Cities).map((city) => (
-          <li className="locations__item" key={nanoid()}>
-            <a className={`locations__item-link tabs__item ${currentCity === city ? `tabs__item--active` : ``}`}
-              onClick={(evt) => {
-                evt.preventDefault();
-                onCityChange(city);
-              }}
-              href="#">
-              <span>{toCamelCase(city)}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
-  </div>
-);
+const CitiesList = ({city: currentCity, onCityChange}) => {
+
+  const getClassNames = (city) => {
+    return classNames({
+      'locations__item-link tabs__item': true,
+      'tabs__item--active': currentCity === city
+    });
+  };
+
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {Object.values(Cities).map((city) => (
+            <li className="locations__item" key={nanoid()}>
+              <a className={getClassNames(city)}
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  onCityChange(city);
+                }}
+                href="#">
+                <span>{toCamelCase(city)}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 CitiesList.propTypes = {
   city: PropTypes.string.isRequired,
