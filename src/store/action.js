5fc105668907a5f2d98c4438f36566f41cd4e60f
Артем Черенков <art.cherenkov@gmail.com>
2020-11-01 18:36:12 +0700

    Создаёт действие LOAD_OFFERS, рефакторит ActionCreator

--- src/store/action.js
+++ src/store/action.js
@@ -3,30 +3,34 @@ export const ActionType = {
   GET_OFFERS: `GET_OFFERS`,
   GET_CITY_OFFERS: `GET_CITY_OFFERS`,
   CHANGE_SORT_TYPE: `CHANGE_SORT_TYPE`,
-  CHANGE_ACTIVE_OFFER: `CHANGE_ACTIVE_OFFER`
+  CHANGE_ACTIVE_OFFER: `CHANGE_ACTIVE_OFFER`,
+  LOAD_OFFERS: `LOAD_OFFERS`,
 };
 
-export const ActionCreator = {
-  changeCity: (newCity) => ({
-    type: ActionType.CHANGE_CITY,
-    payload: newCity,
-  }),
+export const changeCity = (newCity) => ({
+  type: ActionType.CHANGE_CITY,
+  payload: newCity,
+});
 
-  getOffersList: () => ({
-    type: ActionType.GET_OFFERS
-  }),
+export const getOffersList = () => ({
+  type: ActionType.GET_OFFERS,
+});
 
-  getCityOffers: () => ({
-    type: ActionType.GET_CITY_OFFERS
-  }),
+export const getCityOffers = () => ({
+  type: ActionType.GET_CITY_OFFERS,
+});
 
-  changeSortType: (newSortType) => ({
-    type: ActionType.CHANGE_SORT_TYPE,
-    payload: newSortType
-  }),
+export const changeSortType = (newSortType) => ({
+  type: ActionType.CHANGE_SORT_TYPE,
+  payload: newSortType,
+});
 
-  changeActiveOffer: (newActiveOffer) => ({
-    type: ActionType.CHANGE_ACTIVE_OFFER,
-    payload: newActiveOffer
-  })
-};
+export const changeActiveOffer = (newActiveOffer) => ({
+  type: ActionType.CHANGE_ACTIVE_OFFER,
+  payload: newActiveOffer,
+});
+
+export const loadHotels = (hotels) => ({
+  type: ActionType.LOAD_OFFERS,
+  payload: hotels,
+});
