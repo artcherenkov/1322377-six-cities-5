50efa5b5c7e2415f4467cc07e9d5289762959f2e
Артем Черенков <art.cherenkov@gmail.com>
2020-11-01 19:57:20 +0700

    Рефакторит функцию фильтрации офферов по городу
    WIP: Не работает ничего кроме списка офферов и переключения между городами

--- src/core.js
+++ src/core.js
@@ -1,6 +1,8 @@
+import {toCamelCase} from "./utils/common";
+
 export const getCityOffers = (offers, city) => {
   if (offers) {
-    return offers.filter((offer) => offer.city === city);
+    return offers.filter((offer) => offer.city.name === toCamelCase(city));
   }
 
   return [];
