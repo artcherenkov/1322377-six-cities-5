f34735517b9b9a8ab985f0526b5b2a8d4ba3dd06
Артем Черенков <art.cherenkov@gmail.com>
2020-11-01 19:02:01 +0700

    Создаёт селекторы

---
+++ src/store/reducers/app-state/selectors.js
@@ -0,0 +1,4 @@
+export const getCity = (state) => state.STATE.city;
+export const getCityOffers = (state) => state.STATE.cityOffers;
+export const getSortType = (state) => state.STATE.sortType;
+export const getActiveOfferId = (state) => state.STATE.activeOfferId;
