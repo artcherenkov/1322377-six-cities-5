491a3cd523f1eba20820498865ec71d19aec3454
Артем Черенков <art.cherenkov@gmail.com>
2020-11-05 16:37:03 +0700

    Добавляет в сборку пакет history и подключает его

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-
+import {Router as BrowserRouter, Switch, Route} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen.jsx";
 import FavoritesScreen from "../favorites-screen/favorites-screen.jsx";
 import LoginScreen from "../login-screen/login-screen.jsx";
 import OfferScreen from "../offer-screen/offer-screen.jsx";
 import OfferCardProp from '../offer-card/offer-card.prop';
 import PrivateRoute from "../private-route/private-route";
+import browserHistory from "../../browser-history";
 
 const App = ({offers}) => (
-  <BrowserRouter>
+  <BrowserRouter history={browserHistory}>
     <Switch>
       <Route exact path="/">
         <MainScreen offers={offers}/>
