c2f15d0c0be67d81b42f6b3a97710579a507326e
Артем Черенков <art.cherenkov@gmail.com>
2020-11-06 21:56:47 +0700

    Добавляет действие PUSH_ROUTE_TO_REDIRECT

--- src/store/reducers/app-state/selectors.js
+++ src/store/reducers/app-state/selectors.js
@@ -1,3 +1,4 @@
 export const getCity = (state) => state.STATE.city;
 export const getSortType = (state) => state.STATE.sortType;
 export const getActiveOfferId = (state) => state.STATE.activeOfferId;
+export const getRouteToRedirect = (state) => state.STATE.routeToRedirect;
